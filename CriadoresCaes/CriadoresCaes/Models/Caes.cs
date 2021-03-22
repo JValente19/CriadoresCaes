d5f988e63f02ece5f65600a944c730331538d706
Joao Oliveira <aluno21073@ipt.pt>
2021-03-22 16:07:00 +0000

    Início de construção do Modelo - Caes, Racas

---
+++ CriadoresCaes/CriadoresCaes/Models/Caes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CriadoresCaes.Models{
+
+    /// <summary>
+    /// descrição de cada um dos cães
+    /// </summary>
+    public class Caes{
+
+        /// <summary>
+        /// Identificador de cada cão
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do cão
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Sexo do cão
+        /// M - Masculino
+        /// F - Feminino
+        /// </summary>
+        public string Sexo { get; set; }
+
+        /// <summary>
+        /// Data de Nascimento
+        /// </summary>
+        public DateTime Nascimento { get; set; }
+
+        /// <summary>
+        /// Data de Compra
+        /// </summary>
+        public DateTime DataCompra { get; set; }
+
+        /// <summary>
+        /// Registo do cão no Livro de Origem PortuguêsRacas (LOP)
+        /// </summary>
+        public string LOP { get; set; }
+
+    }
+}
